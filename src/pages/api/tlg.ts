e07f09feaedba5d261b5edbc6857436dadbff029
Chicloon <Chicloon@gmail.com>
2023-04-11 19:43:11 +0300

    tgBot added

--- src/pages/api/tlg.ts
+++ src/pages/api/tlg.ts
@@ -1,6 +1,40 @@
-export default async (req, res) => {
-  const tgbot = process.env.NEXT_TELEGRAM_TOKEN;
-  console.log(req.body);
+import TelegramBot from "node-telegram-bot-api";
+import { AppRouter, appRouter } from "~/server/api/root";
+import { getBaseUrl } from "~/utils/api";
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.NEXT_TELEGRAM_TOKEN as string;
+const url = getBaseUrl();
 
-  res.status(200).send("OK");
-};
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+// Matches "/echo [whatever]"
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
+
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
+
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, resp);
+// });
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+
+  // const quiz = appRouter.quiz.getAll();
+  const resp = await fetch(`${url}/api/trpc/quiz.getAll`);
+  const quiz = await resp.json();
+  console.log("🚀 ~ file: tlg.ts:28 ~ bot.on ~ quiz:", quiz);
+  // console.log("msg", msg);
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, "Received your message");
+  // bot.sendMessage(chatId, "Received your message" + ": " + msg);
+  bot.sendMessage(chatId, JSON.stringify(quiz));
+});
+
+export default bot;
